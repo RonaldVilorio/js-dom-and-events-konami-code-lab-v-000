2b49e90a5bd3dbd4db3b0b7b96c935904e766141
ronald vilorio <soccer23415@gmail.com>
2018-05-21 21:45:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,17 +3,5 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init(e) {
   // debugger
   // Write your JavaScript code inside the init() function
-  const key = parseInt(e.detail || e.which);
-
-    if (key === alphabet[index]) {
-      index++;
-
-      if (index === alphabet.length) {
-        alert("Hurray!");
-
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
+  return e.detail
 }
