7f81ac1f1c679cede75e10e91a1a130cb8636d1b
ronald vilorio <soccer23415@gmail.com>
2018-05-21 23:13:49 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,22 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  // debugger
+  let index = 0;
+  document.body.addEventListener('keydown',function(e){
+    const user_input = []
+    const key = parseInt(e.detail || e.which)
 
+    if (key === code[index]){
+      user_input.push(key)
+      index++
+      if (index === code.length){
+        window.alert("hey")
+      }
+
+    }
+
+
+  })
+  // return window.alert("hey")
 }
