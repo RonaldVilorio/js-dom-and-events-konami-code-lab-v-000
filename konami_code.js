a7ad03fd80e9ecb5591e65a9669e938d55da7798
ronald vilorio <soccer23415@gmail.com>
2018-05-21 21:55:11 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init(e) {
-  // debugger
+function init() {
+  debugger
   // Write your JavaScript code inside the init() function
-  return e.detail
+  document.body.getElementById('main')
 }
