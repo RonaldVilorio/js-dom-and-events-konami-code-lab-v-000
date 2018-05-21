06e523f19f48f2d857c16c4ce4130130d5d05b02
ronald vilorio <soccer23415@gmail.com>
2018-05-21 21:57:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,5 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   debugger
   // Write your JavaScript code inside the init() function
-  document.body.getElementById('main')
+  document.getElementById('main').addEventListener('click', function(event) {
+  alert('I was clicked!')
+})
 }
