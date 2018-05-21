5acb801f0c818b968019c282fd40384446b92cfd
ronald vilorio <soccer23415@gmail.com>
2018-05-21 22:23:16 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,5 +3,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   // debugger
+  let index = 0;
+  document.body.addEventListener('keydown',function(e){
+    const key = parseInt(e.detail || e.which)
+  })
   return window.alert("hey")
 }
