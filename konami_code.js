ba678b24e460a0e9fd5b420162f88414511f948b
ronald vilorio <soccer23415@gmail.com>
2018-05-21 22:16:44 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  debugger
+  // debugger
   document.querySelectorAll('div').addEventListener('keydown',function(e){
     if (e.which){
       console.log("congrats")
